c623b8f49cb18594e14f249f2d2034b2da5679ab
kevinderitis <kevin.deritis@hotmail.com>
2023-08-22 17:19:49 -0300

    Double send solved and autoreturn configured

--- src/routes/mp.js
+++ src/routes/mp.js
@@ -24,6 +24,7 @@ mpRouter.get('/pagar', async (req, res) => {
             quantity: 1,
           },
         ],
+        auto_return: "approved",
         back_urls: {
           success: `${config.DOMAIN_URL}/api/mp/callbackURL`,
           failure: `${config.DOMAIN_URL}/api/mp/failed-payment`
@@ -61,37 +62,6 @@ mpRouter.get('/pagar', async (req, res) => {
     adjuntos.push({ path: './Prompt-engineering.pdf' })
 
     res.redirect(config.SUCCESFULL_PAYMENT_URL)
-      try {
-        await sendMail({
-          to: email,
-          subject: 'Libreria digital - Prompt engineering',
-          attachments: adjuntos,
-          html: `<!DOCTYPE html>
-          <html>
-          <head>
-            <title>Gracias por tu compra</title>
-          </head>
-          <body>
-            <div style="text-align: center; padding: 20px;">
-              <h2>¡Gracias por tu compra!</h2>
-             
-              <p>Queremos agradecerte sinceramente por haber adquirido nuestro curso de Prompt Engineering. Estamos emocionados de que hayas decidido unirte a nosotros en este emocionante viaje de aprendizaje y desarrollo.</p>
-              <p>El curso está diseñado para brindarte conocimientos prácticos y valiosos en el campo de la ingeniería y la programación. Esperamos que encuentres el contenido interesante y útil para tu crecimiento profesional.</p>
-              <p>Si tienes alguna pregunta o necesitas asistencia durante el curso, no dudes en ponerte en contacto con nuestro equipo de soporte. Estamos aquí para ayudarte en cada paso del camino.</p>
-              <p>Una vez más, gracias por confiar en nosotros.</p>
-              <p>Saludos cordiales,</p>
-              <p>El equipo de Libreria digital</p>
-            </div>
-          </body>
-          </html>
-          `
-        });
-
-        await updateRecord(email)
-      } catch (error) {
-        console.log(error)
-      }
-  
   });
 
   mpRouter.get('/failed-payment', async (req, res) => {
